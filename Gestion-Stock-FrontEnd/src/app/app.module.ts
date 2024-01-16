2793b7423b2d4f2a9aba37e31692cc5e463e27d5
Farida <farida.chelhi06@gmail.com>
2024-01-16 10:32:53 +0100

    ajout de composant page-incription

--- Gestion-Stock-FrontEnd/src/app/app.module.ts
+++ Gestion-Stock-FrontEnd/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { PageInscriptionComponent } from './pages/page-inscription/page-inscription.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    PageInscriptionComponent
   ],
   imports: [
     BrowserModule,
