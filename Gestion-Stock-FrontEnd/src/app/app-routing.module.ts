956d65a94550732c617d7ae1c6e1331889e06d67
Farida <farida.chelhi06@gmail.com>
2024-01-16 15:52:42 +0100

    ajout de decoupage de dashboard et le routage de page-statistique

--- Gestion-Stock-FrontEnd/src/app/app-routing.module.ts
+++ Gestion-Stock-FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { PageDashboardComponent } from './pages/page-dashboard/page-dashboard.component';
+import { PageInscriptionComponent } from './pages/page-inscription/page-inscription.component';
+import { PageStatistiqueComponent } from './pages/page-statistique/page-statistique.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'login',
+    component: LoginPageComponent
+  },
+  {
+    path:'inscrire',
+    component: PageInscriptionComponent
+  },
+  {
+    path:'',
+    component: PageDashboardComponent,
+    children:[
+      {
+        path:'statistiques',
+        component: PageStatistiqueComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Gestion-Stock-FrontEnd/src/app/app.module.ts
+++ Gestion-Stock-FrontEnd/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { PageInscriptionComponent } from './pages/page-inscription/page-inscription.component';
+import { PageDashboardComponent } from './pages/page-dashboard/page-dashboard.component';
+import { PageStatistiqueComponent } from './pages/page-statistique/page-statistique.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
-    PageInscriptionComponent
+    PageInscriptionComponent,
+    PageDashboardComponent,
+    PageStatistiqueComponent
   ],
   imports: [
     BrowserModule,
